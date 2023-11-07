482c5b545c0cfbfc2bf27fa4faf13ba5e2618742
Ansh Gupta <guptaansh557@gmail.com>
2023-11-07 18:55:43 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,43 @@
+const {MongoClient} = require('mongodb')
+
+let dbConnection;
+
+module.exports = {
+    connectToDb: (cb) =>{
+        MongoClient.connect(/*'mongodb://127.0.0.1:27017/bookstore'*/ 'mongodb+srv://anshg0432:f8DRWunmH1Xje6Nc@cluster0.mrhin2q.mongodb.net/mongo_app?retryWrites=true&w=majority')
+            .then((client)=>{
+                dbConnection = client.db()
+                return cb()
+            }).catch((err)=>{
+                console.log(err)
+                return cb(err)
+            })
+    },
+    
+    getDb: () => dbConnection
+}
+
+// const { MongoClient } = require('mongodb');
+
+// let dbConnection;
+
+// module.exports = {
+//   connectToDb: (cb) => {
+    
+//     // Replace the connection URL with your MongoDB server's URL
+//     const uri = 'mongodb://127.0.0:27017/bookstore';
+    
+//     const client = new MongoClient(uri);
+
+//     client.connect()
+//       .then(() => {
+//         dbConnection = client.db();
+//         return cb();
+//       })
+//       .catch((err) => {
+//         console.error('Error connecting to MongoDB:', err);
+//         return cb(err);
+//       });
+//   },
+//   getDb: () => dbConnection,
+// };
